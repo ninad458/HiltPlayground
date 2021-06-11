5655c58236eb09e6d2a543d5a6569a99615a9bc7
enigma <ninadthakare458@gmail.com>
2021-06-11 20:57:47 +0530

    ViewModelScoped MainComputation

---
+++ app/src/main/java/com/enigma/playground/hilt/main/MainComputation.kt
@@ -0,0 +1,8 @@
+package com.enigma.playground.hilt.main
+
+import dagger.hilt.android.scopes.ViewModelScoped
+import javax.inject.Inject
+
+@ViewModelScoped
+class MainComputation @Inject constructor() {
+}
