02dfb09737402f23adec88de72fdf48ca0544bc3
enigma <ninadthakare458@gmail.com>
2021-06-11 20:53:17 +0530

    shared preference remove inject

--- app/src/main/java/com/enigma/playground/hilt/data/SharedPrefs.kt
+++ app/src/main/java/com/enigma/playground/hilt/data/SharedPrefs.kt
@@ -3,4 +3,4 @@ package com.enigma.playground.hilt.data
 import android.content.Context
 import javax.inject.Inject
 
-class SharedPrefs @Inject constructor(private val context: Context)
+class SharedPrefs constructor(private val context: Context)
