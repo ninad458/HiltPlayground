adf7a17dc393891b550a09fc8e5986d0372896a8
enigma <ninadthakare458@gmail.com>
2021-06-11 17:56:32 +0530

    ad hoc initialisation

--- app/src/main/java/com/enigma/playground/hilt/App.kt
+++ app/src/main/java/com/enigma/playground/hilt/App.kt
@@ -1,5 +1,21 @@
 package com.enigma.playground.hilt
 
 import android.app.Application
+import android.content.Context
+import com.enigma.playground.hilt.data.LocalDb
 
-class App: Application()
+class App : Application() {
+
+    private lateinit var localDb: LocalDb
+
+    fun getLocalDb(): LocalDb {
+        if (!::localDb.isInitialized) {
+            localDb = createLocalDb(this)
+        }
+        return localDb
+    }
+
+    companion object {
+        fun createLocalDb(context: Context) = LocalDb(context.applicationContext)
+    }
+}
--- app/src/main/java/com/enigma/playground/hilt/main/MainActivity.kt
+++ app/src/main/java/com/enigma/playground/hilt/main/MainActivity.kt
@@ -3,12 +3,12 @@ package com.enigma.playground.hilt.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.enigma.playground.hilt.App
 import com.enigma.playground.hilt.R
 import com.enigma.playground.hilt.analytics.AnalyticsService
 import com.enigma.playground.hilt.analytics.FacebookAnalytics
 import com.enigma.playground.hilt.analytics.MixPanelAnalytics
 import com.enigma.playground.hilt.data.ApiService
-import com.enigma.playground.hilt.data.LocalDb
 import com.enigma.playground.hilt.data.Repository
 import com.enigma.playground.hilt.data.SharedPrefs
 
@@ -20,7 +20,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val apiService = ApiService.getInstance()
-        val localDb = LocalDb(this.applicationContext)
+
+        val localDb = (applicationContext as? App)?.getLocalDb() ?: App.createLocalDb(this)
         val sharedPrefs = SharedPrefs(this.applicationContext)
         val mixPanelAnalytics = MixPanelAnalytics()
         val facebookAnalytics = FacebookAnalytics()
